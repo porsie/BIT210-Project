315d9e194138520e2c676b3bb1755a4590debd0b
porsie <86921293+porsie@users.noreply.github.com>
2023-03-22 18:14:01 +0800

    Database connection & dashboard for HR

---
+++ footer.php
@@ -0,0 +1,16 @@
+</div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+        <div class="footer">
+            <small><i>Copyright &copy; 2023 HELP University</i></small>
+        </div>
+
+        <!-- JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" 
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" 
+        integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+         <script src="PorSie.js"></script>
+    </body>
+</html>
