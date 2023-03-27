ea93d4c5b5e6f28e06024d3a303a1c18b0567160
Fung Lok Then <gem.ice.fantasy@gmail.com>
2023-03-27 10:55:11 +0800

    //

---
+++ supervisorDashboard.php
@@ -0,0 +1,25 @@
+<?php include 'header.php';
+include 'db.php';
+session_start(); 
+
+if (isset($_SESSION['employeeID']) && isset($_SESSION['name'])) {
+    ?>
+
+
+<main>
+    <hr>
+    <h3>Welcome to the Supervisor Dashboard</h3>
+    <label for="name"> <?php echo "Supervisor Name : $_SESSION[name] ";?> </label><br><br><br>
+    <label for="empID"><?php echo "Supervisor ID : $_SESSION[employeeID] ";?> </label><br><br><br>
+    <label for="position">Position: Supervisor</label><br><br><br>
+    <label for="email">Email: HRAdmin@gmail.com </label><br><br><br> // email need retrieve from database
+
+    <button type="button" class="btn btn-success">Edit Profile</button>
+
+        
+</main>
+<br>
+<?php
+ include 'footer.php'; 
+}
+?>
