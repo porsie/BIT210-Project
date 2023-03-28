b998595a9b3c66ae15c83831a24e91a778a4cc04
Fung Lok Then <gem.ice.fantasy@gmail.com>
2023-03-28 18:49:34 +0800

    //

--- db.php
+++ db.php
@@ -1,6 +1,10 @@
 <?php
 
+<<<<<<< Updated upstream
 $db= new mysqli("localhost","root","","mywebpro", "3308");
+=======
+$db= new mysqli("localhost","root","","bit210assg", "3308");
+>>>>>>> Stashed changes
 
 if ($db -> connect_error) {
     echo "Failed to connect to MySQL: " . $db -> connect_error;
--- ps2/header.php
+++ ps2/header.php
@@ -25,8 +25,13 @@
                     <nav>
                         <div class="row row-cols-5 row-cols-md-1">
                             <div class="col py-3 text-center text-md-left">Menu</div>
+<<<<<<< Updated upstream:ps2/header.php
                             <div class="col py-3 text-center text-md-left"><a href="RegisterEmployee.php">Register Employee</a></div>
                             <div class="col py-3 text-center text-md-left"><a href="">View FWA Analytics</a></div>
+=======
+                            <div class="col py-3 text-center text-md-left"><a href="SubmitFWA.php">Submit FWA Request</a></div>
+                            <div class="col py-3 text-center text-md-left"><a href="UpdateDailySchedule.php">Update Daily Schedule</a></div>
+>>>>>>> Stashed changes:headerEmployee.php
                             <div class="col py-3 text-center text-md-left"><a href="logout.php">Logout</a></div>
                         </div>
                     </nav>
